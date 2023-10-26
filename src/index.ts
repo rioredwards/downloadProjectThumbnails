e9a853db608aa42eef36e4cbb85155281d3d93c1
Rio Edwards <rioredwards@gmail.com>
2023-10-26 00:09:50 -0700

    Initial

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+// This script will download the preview images for all projects in the contentful CMS
+// and save them to the local filesystem
+
+import { getCodeProjectCardsContent } from './api.js';
+import convertGifToPng from 'gif-png-converter';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const baseDir = process.argv[2] || process.env.CONTENTFUL_PROJECT_PREVIEW_IMAGES_DIR;
+if (!baseDir) {
+  console.error('Please provide a directory to save the images to');
+  process.exit(1);
+}
+
+export default async function main(baseDir: string) {
+  // Fetch the project content from the contentful CMS
+  const projectCardContent = await getCodeProjectCardsContent();
+
+  // Store the paths to the downloaded images
+  const newImagePaths: string[] = [];
+
+  // For each project, download the convert the gif into a png
+  // and download the png
+  for (const project of projectCardContent) {
+    const previewImageUrl = project.preview.url;
+
+    // Skip projects without a preview image
+    if (!previewImageUrl) continue;
+
+    const previewImageFilename = previewImageUrl.split('/').pop() as string;
+    const filePath = `${baseDir}/${project.slug}/${previewImageFilename}`;
+
+    console.log(`Downloading ${previewImageUrl} to ${filePath}`);
+
+    try {
+      await convertGifToPng(previewImageUrl, filePath);
+      newImagePaths.push(filePath);
+    } catch (err) {
+      console.error(`Error downloading ${previewImageUrl}`, err);
+      process.exit(1);
+    }
+  }
+}
+
+main(baseDir);
